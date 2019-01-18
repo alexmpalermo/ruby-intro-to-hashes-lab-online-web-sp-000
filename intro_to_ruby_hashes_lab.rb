2a4a00f44110494212d80a6369d7f0610a73e243
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-18 18:09:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,9 @@ def monopoly_with_third_tier
 		monopoly = {:railroads => {
 	  :pieces => 4,
 	  :names => {
-	    :reading_railroad => {}
+	    :reading_railroad => {},
+	    :pennsylvania_railroad => {},
+	    
 	    
 	  },
 	  :rent_in_dollars => {
